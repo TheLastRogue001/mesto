4a672f284e7b114571f2d44c9a1ce337d6dd61c3
Alex Shevelkov <sasha.shevelkov0@yandex.ru>
2023-05-05 10:59:29 +0300

    fix: add function input

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,46 @@
-(function(){
-    let modal = document.querySelector('.profile__form');
-    let closeButton = document.querySelector('.profile__form-close');
-    let modalTriggers = document.querySelectorAll('[data-trigger]');
-  
-    let isModalOpen = false;
-    let pageYOffset = 0;
-  
-    const openModal = function() {
-      pageYOffset = window.pageYOffset;
-      modal.classList.add('is-open');
-      isModalOpen = true;
+(function () {
+  let modal = document.querySelector(".profile__form");
+  let closeButton = document.querySelector(".profile__form-close");
+  let formElement = document.getElementById("submit");
+  let modalTriggers = document.querySelectorAll("[data-trigger]");
+
+  let isModalOpen = false;
+  let pageYOffset = 0;
+
+  const openModal = function () {
+    pageYOffset = window.pageYOffset;
+    modal.classList.add("is-open");
+    isModalOpen = true;
+  };
+
+  const closeModal = function () {
+    modal.classList.remove("is-open");
+    isModalOpen = false;
+  };
+
+  const onScroll = function (e) {
+    if (isModalOpen) {
+      e.preventDefault();
+      window.scrollTo(0, pageYOffset);
     }
-  
-    const closeModal = function() {
-      modal.classList.remove('is-open');
-      isModalOpen = false;
-    }
-  
-    const onScroll = function(e) {
-      if (isModalOpen) {
-        e.preventDefault();
-        window.scrollTo(0, pageYOffset);
-      }
-    }
-  
-    modalTriggers.forEach(function(item) { 
-      item.addEventListener('click', openModal);
-    })
-  
-    document.addEventListener('scroll', onScroll);
-  
-    closeButton.addEventListener('click', closeModal);
-  })();
+  };
+
+  const handleFormSubmit = function (evt) {
+    evt.preventDefault();
+    document.querySelector(".profile__title").innerHTML =
+      document.getElementById("name_input").value;
+    document.querySelector(".profile__subtitle").innerHTML =
+      document.getElementById("job_input").value;
+    closeModal();
+  };
+
+  modalTriggers.forEach(function (item) {
+    item.addEventListener("click", openModal);
+  });
+
+  document.addEventListener("scroll", onScroll);
+
+  formElement.addEventListener("click", handleFormSubmit);
+
+  closeButton.addEventListener("click", closeModal);
+})();
