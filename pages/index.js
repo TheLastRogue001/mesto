2429afa2cf7ff6316cc2efbc129711ac202066ec
Alex Shevelkov <sasha.shevelkov0@yandex.ru>
2023-05-10 20:57:48 +0300

    fix: modal js

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,53 @@
-(function () {
-  let modal = document.querySelector(".profile__form");
-  let closeButton = document.querySelector(".profile__form-close");
-  let formElement = document.getElementById("submit");
-  let modalTriggers = document.querySelectorAll("[data-trigger]");
+let content = document.querySelector(".content");
+let profileFormContainer = content.querySelector(".profile__form-container");
+let editButton = content.querySelector(".profile__edit-button");
 
-  let isModalOpen = false;
-  let pageYOffset = 0;
+function openForm() {
+  profileFormContainer.insertAdjacentHTML(
+    "beforeend",
+    `
+    <div class="profile__form">
+      <div class="profile__form-content">
+        <h2 class="profile__form-title">Редактировать профиль</h2>
+        <input
+          class="profile__form-input profile__form-input-title"
+          type="text"
+          value="Жак-Ив Кусто"
+        />
+        <input
+          class="profile__form-input profile__form-input-subtitle"
+          type="text"
+          value="Исследователь океана"
+        />
+        <button type="submit" class="profile__form-save">Сохранить</button>
+      </div>
+      <button class="profile__form-close"></button>
+    </div>
+  `
+  );
+  let saveButton = profileFormContainer.querySelector(".profile__form-save");
+  let closeButton = profileFormContainer.querySelector(".profile__form-close");
+  closeButton.addEventListener("click", closeForm);
+  saveButton.addEventListener("click", handleFormSubmit);
+}
 
-  const openModal = function () {
-    pageYOffset = window.pageYOffset;
-    modal.classList.add("profile__form-open");
-    isModalOpen = true;
-  };
+function closeForm() {
+  let closeButton = profileFormContainer.querySelector(".profile__form-close");
+  closeButton.parentNode.remove();
+}
 
-  const closeModal = function () {
-    modal.classList.remove("profile__form-open");
-    isModalOpen = false;
-  };
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  let titleName = content.querySelector(".profile__title");
+  let inputName = content.querySelector(".profile__form-input-title").value;
+  let subtitleJob = content.querySelector(".profile__subtitle");
+  let inputSubtitle = content.querySelector(
+    ".profile__form-input-subtitle"
+  ).value;
+  titleName.textContent = inputName;
+  subtitleJob.textContent = inputSubtitle;
+  closeForm();
+}
 
-  const onScroll = function (e) {
-    if (isModalOpen) {
-      e.preventDefault();
-      window.scrollTo(0, pageYOffset);
-    }
-  };
-
-  const handleFormSubmit = function (evt) {
-    evt.preventDefault();
-    document.querySelector(".profile__title").innerHTML =
-      document.getElementById("name_input").value;
-    document.querySelector(".profile__subtitle").innerHTML =
-      document.getElementById("job_input").value;
-    closeModal();
-  };
-
-  modalTriggers.forEach(function (item) {
-    item.addEventListener("click", openModal);
-  });
-
-  document.addEventListener("scroll", onScroll);
-
-  formElement.addEventListener("click", handleFormSubmit);
-
-  closeButton.addEventListener("click", closeModal);
-})();
+editButton.addEventListener("click", openForm);
+profileFormContainer.addEventListener("submit", handleFormSubmit);
