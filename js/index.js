da21edec24ce9e7faf34b1ec46f831d12aa23612
Alex Shevelkov <sasha.shevelkov0@yandex.ru>
2023-05-15 19:35:52 +0300

    fix: pages

--- js/index.js
+++ js/index.js
@@ -1,22 +1,21 @@
-let page = document.querySelector(".page");
 let content = document.querySelector(".content");
 let popup = document.querySelector(".popup");
 let editButton = content.querySelector(".profile__edit-button");
 let titleName = content.querySelector(".profile__title");
 let subtitleJob = content.querySelector(".profile__subtitle");
-let inputName = popup.querySelector(".popup__input_name");
-let inputSubtitle = popup.querySelector(".popup__input_job");
+let inputName = popup.querySelector(".popup__input:nth-child(1)");
+let inputSubtitle = popup.querySelector(".popup__input:nth-child(2)");
 let closeButton = popup.querySelector(".popup__close");
 let formSubmit = popup.querySelector(".popup__form");
 
 function openPopup() {
+  inputName.value = titleName.textContent;
+  inputSubtitle.value = subtitleJob.textContent;
   popup.classList.add("popup_opened");
-  page.classList.add("page_overflow_hiden");
 }
 
 function closePopup() {
   popup.classList.remove("popup_opened");
-  page.classList.remove("page_overflow_hiden");
 }
 
 function handleFormSubmit(evt) {
