2c53bb581c5498af3c27b0723f18cb6e284f83a5
Alex Shevelkov <sasha.shevelkov0@yandex.ru>
2023-05-16 22:54:49 +0300

    fix: add modal add-card

--- js/index.js
+++ js/index.js
@@ -1,30 +1,82 @@
-let content = document.querySelector(".content");
-let popup = document.querySelector(".popup");
-let editButton = content.querySelector(".profile__edit-button");
-let titleName = content.querySelector(".profile__title");
-let subtitleJob = content.querySelector(".profile__subtitle");
-let inputName = popup.querySelector('#input-name');
-let inputSubtitle = popup.querySelector("#input-job");
-let closeButton = popup.querySelector(".popup__close");
-let formSubmit = popup.querySelector(".popup__form");
+const content = document.querySelector(".content");
 
-function openPopup() {
+// Modal
+const popup = document.querySelector(".popup");
+const popupContainerTemplate = popup.querySelector("#popup__container").content;
+const popupContainer = popupContainerTemplate
+  .querySelector(".popup__container")
+  .cloneNode(true);
+
+// Title
+const titleName = content.querySelector(".profile__title");
+const subtitleJob = content.querySelector(".profile__subtitle");
+
+// Input
+const inputName = popupContainer.querySelector("#input-name");
+const inputSubtitle = popupContainer.querySelector("#input-info");
+
+// Button
+const editButton = content.querySelector(".profile__edit-button");
+const addCardButton = content.querySelector(".profile__add-button");
+const closeButton = popupContainer.querySelector(".popup__close");
+
+// Form
+const formSubmit = popupContainer.querySelector('[name="popup"]');
+
+let clickedEditProfile = false;
+let clickedAddCard = false;
+
+function openPopupEditProfile() {
+  clickedEditProfile = true;
+  popup.classList.add("popup_opened");
+  popupContainer.querySelector(".popup__title").textContent =
+    "Редактировать профиль";
+  popupContainer.querySelector(".popup__save").textContent = "Сохранить";
+  inputName.placeholder = "ФИО";
+  inputSubtitle.placeholder = "Работа";
   inputName.value = titleName.textContent;
   inputSubtitle.value = subtitleJob.textContent;
+  popup.append(popupContainer);
+}
+
+function openPopupAddCard() {
+  clickedAddCard = true;
   popup.classList.add("popup_opened");
+  popupContainer.querySelector(".popup__title").textContent = "Новое место";
+  popupContainer.querySelector(".popup__save").textContent = "Создать";
+  inputName.placeholder = "Название";
+  inputSubtitle.placeholder = "Ссылка на картинку";
+  inputName.value = "";
+  inputSubtitle.value = "";
+  popup.append(popupContainer);
 }
 
 function closePopup() {
+  clickedEditProfile = false;
+  clickedAddCard = false;
   popup.classList.remove("popup_opened");
 }
 
-function handleFormSubmit(evt) {
-  evt.preventDefault();
-  titleName.textContent = inputName.value;
-  subtitleJob.textContent = inputSubtitle.value;
-  closePopup();
+function handleFormSubmitEditProfile(evt) {
+  if (clickedEditProfile) {
+    evt.preventDefault();
+    titleName.textContent = inputName.value;
+    subtitleJob.textContent = inputSubtitle.value;
+    closePopup();
+  }
+  clickedEditProfile = false;
 }
 
-editButton.addEventListener("click", openPopup);
+function handleFormSubmitAddCard(evt) {
+  if (clickedAddCard) {
+    evt.preventDefault();
+    closePopup();
+  }
+  clickedAddCard = false;
+}
+
+addCardButton.addEventListener("click", openPopupAddCard);
+editButton.addEventListener("click", openPopupEditProfile);
 closeButton.addEventListener("click", closePopup);
-formSubmit.addEventListener("submit", handleFormSubmit);
+formSubmit.addEventListener("submit", handleFormSubmitEditProfile);
+formSubmit.addEventListener("submit", handleFormSubmitAddCard);
